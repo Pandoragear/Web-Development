bdda5a83c4363e39f77dcf0c32f74c9351e67fe1
Andy Dang <andy.dang.it@gmail.com>
2020-12-08 20:23:11 -0600

    Behaviour to Websites

---
+++ Javascript/index.js
@@ -0,0 +1,14 @@
+// This will let you create multi line of code in Chrome. 
+// Chrome => Developer => Sources => Snippets => new file 
+
+// full name of alert
+window.alert("hello World");
+
+// short version of alert 
+alert("hello!");
+alert("World");
+
+
+// alert => function 
+// ("Hello") => Message 
+// ; => END 
