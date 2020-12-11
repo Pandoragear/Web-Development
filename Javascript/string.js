8a67e7629d4251a32d33ab888a7383be782b737f
Andy Dang <andy.dang.it@gmail.com>
2020-12-10 20:39:28 -0600

    String  Concatenation

---
+++ Javascript/string.js
@@ -0,0 +1,15 @@
+// String Concatenation 
+
+// alert ("hello " + "World");
+
+var message = "Hello";
+var name = "Angela";
+
+//Write your code below this line: 
+
+name = prompt("What is your name");
+alert= ( message + " " + name);
+
+
+//Soultion 
+alert(message + " there," + " " + name); 
