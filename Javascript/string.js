5fdd7475aecb9d5526945279381ed4c617988932
Andy Dang <andy.dang.it@gmail.com>
2020-12-10 21:14:24 -0600

    String + Num challenge

--- Javascript/string.js
+++ Javascript/string.js
@@ -13,3 +13,22 @@ alert= ( message + " " + name);
 
 //Soultion 
 alert(message + " there," + " " + name); 
+
+/////////////////////////////////////////////////////////////
+
+
+// string challenge 
+
+var message = ""; 
+let char = 240
+message = prompt("Write your message and you only have " + 240 + " character ")
+message = message.length
+
+let total = char- message
+alert ("you have written " + message + " characters," + " you have " + total + " characters left.");
+
+// Solution 
+var tweet = prompt ("compose your tweet:");
+var tweetCount = tweet.length;
+
+alert("you have written " + tweetCount + " characters, you " + (140 - tweetCount) + " charactes remaining.");
