224c43b4d24d8a0da425791cc6ac4260def8bb67
Andy Dang <andy.dang.it@gmail.com>
2020-12-16 21:09:39 -0600

    Solution  Changing String Casing

--- Javascript/codeChallenge.js
+++ Javascript/codeChallenge.js
@@ -25,3 +25,27 @@ second = second.toLowerCase();
 alert("Hello " + first + second);
 
 
+// Solution 
+
+// 1 Create a var that stores the name that user enters via prompt. 
+let name = prompt ("What is your name?");
+
+// 2 Capitalize the first letter of their name. 
+
+// a isolate the first character
+var firstChar = name.slice(0,1);
+
+// b Turn the first char to Upper Case.
+var upperCaseFirstChar = firstChar.toUpperCase();
+
+// c Isolate the rest of the name 
+var restOfName = name.slice(1, name.length);
+
+// d Change the rest of the name to Lower Case
+restOfName = restOfName.toLowerCase();
+
+// e concactenate the frist char with the rest of the characters
+var capitalizedName = upperCaseFirstChar + restOfName;
+
+// 3 We use the capitalized version of their name to greet them using an alert. 
+alert("Hello, " + capitalizedName)
