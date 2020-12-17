5058d25fc481565c9263659a0d266d141d58f86f
Andy Dang <andy.dang.it@gmail.com>
2020-12-16 20:58:24 -0600

    CC my code

--- Javascript/codeChallenge.js
+++ Javascript/codeChallenge.js
@@ -15,3 +15,13 @@ console.log("a is " + a);
 console.log("b is " + b);
 
 
+////////////////////////////////////////////////////////
+// crate an prompt then alert and change it to only the frist letter to be capital 
+var name = prompt("what is your name");
+first = name.slice(0,1);
+second = name.slice(1,12);
+first = first.toUpperCase();
+second = second.toLowerCase();
+alert("Hello " + first + second);
+
+
