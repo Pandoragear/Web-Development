fc949c1b646f14b95f6f5b54a250c04cce9aa9a8
Andy Dang <andy.dang.it@gmail.com>
2020-12-29 17:33:25 -0600

    increment Increase

--- Javascript/arithmetic.js
+++ Javascript/arithmetic.js
@@ -2,4 +2,10 @@
 
 let dogAge = prompt("How old is your dog");
 let humanAge = (dogAge -2) * 4 + 21;
-alert(humanAge);
+alert("Your dog is  " + humanAge + " years old in human years");
+
+
+let x = 5; 
+let y =3;
+x++;
+x+=y;
