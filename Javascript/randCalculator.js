ff9e64d90c0d90d9b402c443110e3fd2fdb57d01
pandoragear <andy.dang.it@gmail.com>
2021-01-14 21:52:19 -0600

    Update Random Calculator

--- Javascript/randCalculator.js
+++ Javascript/randCalculator.js
@@ -1,4 +1,14 @@
-var n = Math.random();
-n *= 6; 
-n = Math.floor(n) + 1;
-console.log(n);
+// Love Calculator
+
+
+let first = prompt("What is the 1st name"); // fake prompt
+let second = prompt("what is 2nd person name?"); // fake prompt
+
+var n = Math.random(); // range 0 - 0.9999
+
+n *= 100; // scale it which number 0 - 100
+
+n = Math.floor(n) + 1; // 1 - 6
+
+alert("compatibility " + n + " %");
+
