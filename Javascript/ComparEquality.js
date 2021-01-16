f0959e8e1be70b90c170d13aa58934ab8e610bde
pandoragear <andy.dang.it@gmail.com>
2021-01-16 08:13:05 -0600

    Update Combing Comparators

--- Javascript/ComparEquality.js
+++ Javascript/ComparEquality.js
@@ -21,4 +21,24 @@ if (a === b) {
     console.log('no');
 }
 
+/////////////////////////////////////////////////////////////
+
+let first = prompt("What is the 1st name");
+let second = prompt("what is 2nd person name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if(loveScore > 70){
+    alert("compatibility " + loveScore + " %" + " You love each other like Kanye loves Kanye.");
+}
+if (loveScore > 30 && loveScore <=70){
+    alert("compatibility " + loveScore + " %" + " ehhh");
+}
+
+if (loveScore <= 30){
+    alert(" compatibility " + loveScore +"%" + " run, run far away boy");
+} else {
+    
+};
 
