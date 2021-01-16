a42819bfedcc07c3284d447877244052e638ad64
pandoragear <andy.dang.it@gmail.com>
2021-01-15 23:12:20 -0600

    Update If Else Logic

--- Javascript/ifElse.js
+++ Javascript/ifElse.js
@@ -1,5 +1,17 @@
-if ( track === "clear") {
-    goStraight ();
+// conditional logic for If else
+
+let first = prompt("What is the 1st name");
+let second = prompt("what is 2nd person name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if(loveScore > 70){
+    alert("compatibility " + loveScore + " %" + " You love each other like Andy loves thao");
 }else {
-    turnRight();
-}
+    alert("compatibility " + loveScore + " %" + " ehhhhhhh");
+};
+
+
+// 1st alert will show if random generate number is over 70
+// 2nd alert will show if random genrate number is under 70 
