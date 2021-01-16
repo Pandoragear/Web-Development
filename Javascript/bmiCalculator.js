13df5345df85900c831a78d275cd8549c1cd3ad4
pandoragear <andy.dang.it@gmail.com>
2021-01-16 08:55:54 -0600

    Update Code Challenge

--- Javascript/bmiCalculator.js
+++ Javascript/bmiCalculator.js
@@ -1,9 +1,24 @@
 // BMI = weight / height(2) (m(2))
 
-function bmiCalculator(weight, height){
-    // var bmi = weight / (height * height);
-    var bmi = weight / Math.pow(height, 2)
-    return Math.round(bmi);
-}
+function bmiCalculator (weight, height) {
 
-bmiCalculator(65, 1.8);
+    var interpreation; 
+
+    var bmi = weight / Math.pow(height, 2);
+
+    bmi = Math.round(bmi);
+    
+    if ( bmi < 18.5 ){
+    return "your BMI is " + bmi + " so you are underweight"
+    } 
+    if (bmi> 18.5 && bmi <= 24.9){
+    return  " your BMI is" + bmi + " so you have a normal weight. "
+    } 
+    else{ (bmi > 24.9) 
+    return " your BMI is" + bmi + " so you are overweight."
+    }
+    
+    return interpreation;
+};
+
+bmiCalculator(60, 2);
