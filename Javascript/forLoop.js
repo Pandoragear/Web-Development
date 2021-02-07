b021e0cdb58abdf6e1cfeee7a2b1fc453058811a
pandoragear <andy.dang.it@gmail.com>
2021-02-06 23:12:31 -0600

    Add function fizzBuzz as forLoop

--- Javascript/forLoop.js
+++ Javascript/forLoop.js
@@ -1,7 +1,30 @@
-for (i = 0; i< 2; i++ ) {
-    // do something 
-}
+// for (i = 0; i< 2; i++ ) {
+//     // do something 
+// }
 
 // i = 0; {start}
 // i< 2; {end}
 // i++; {change}
+
+
+for (var i =1; i<2; i++) {
+    console.log(i);
+}
+
+var output =[];
+
+function fizzBuzz(){
+    for (var count = 1; count > 101; count++) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push ("FizzBuzz");
+        } else if (count % 3 === 0){
+            output.push("Fizz");
+        } else if (count % 5 === 0) {
+            output.push("Buzz");
+        }else {
+            output.push(count);
+        }
+
+    }
+    console.log(output);
+}
