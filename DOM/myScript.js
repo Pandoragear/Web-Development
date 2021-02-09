1df7bc1b6530a868fab6eb1665f71c060e1938b5
pandoragear <andy.dang.it@gmail.com>
2021-02-08 23:43:54 -0600

    Add QuerySelector

--- DOM/myScript.js
+++ DOM/myScript.js
@@ -1,1 +1,10 @@
 // document.querySelector("h1").innerHTML = "Goob Bye";
+
+// This target tag name 
+// document.getElementsByTagName("li")[2].style.color = "red";
+
+// This target Element class 
+// document.getElementsByClassName("btn")[0].style.color = "yellow";
+
+// this target any that you select 
+// document.querySelector("a").style.color ="Green"; 
