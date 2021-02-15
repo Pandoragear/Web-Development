065523a56294132c519e6de748758be509f16e31
pandoragear <andy.dang.it@gmail.com>
2021-02-14 19:52:43 -0600

    Constructor naming

---
+++ Javascript/constructor.js
@@ -0,0 +1,7 @@
+// Constructor the name have to be capital 
+function BellBoy( name, age, hasWorkPermit, languages ) {
+    this.name = name; 
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages; 
+}
