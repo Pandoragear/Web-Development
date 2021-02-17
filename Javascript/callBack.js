0757c68d148112908f1859141f109eb1ae7e4df3
pandoragear <andy.dang.it@gmail.com>
2021-02-16 23:29:57 -0600

    Add another callBack function

--- Javascript/callBack.js
+++ Javascript/callBack.js
@@ -3,3 +3,9 @@ document.addEventListener("keypress", respondToKey(event));
 function respondToKey(event) {
     console.log("keypressed.");
 }
+
+
+//this change the name on Wiki
+// $0.addEventListener("click", function(event ){
+//     console.log();
+// })
