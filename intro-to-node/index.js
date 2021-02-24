7a97de430989abe1f7a7e6f4579718ebf32ba408
Andy Dang <andy.dang.it@gmail.com>
2021-02-24 17:54:58 -0600

    Update package supervillains

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -8,9 +8,12 @@
 
 
 const superheroes = require ('superheroes');
+const supervillains = require('supervillains');
 
+
+var myVillainsName = supervillains.random();
 var myHeroNames = superheroes.random();
 
+console.log("my day time name is: " + myHeroNames + " and my night persona is: " + myVillainsName);
 
-console.log(myHeroNames);
 
