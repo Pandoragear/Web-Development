9b873e8987f8937222e30fc1d82d3124376c74e0
Andy Dang <andy.dang.it@gmail.com>
2021-02-24 21:24:45 -0600

    Add app.get contact/about

--- My-Express-Server/server.js
+++ My-Express-Server/server.js
@@ -6,6 +6,14 @@ app.get("/", function(req, res){
     res.send("<h1> Hello, World!! </h1>");
 });
 
+app.get("/contact", function(req, res){
+    res.send("Contact me at: andy.dang.it@gmail.com")
+});
+
+app.get("/about", function(req, res){
+    res.send("Name is Andy Dang and I will become a software developer");
+});
+
 app.listen(3000, function(){
     console.log("server started on port 3000");
 });
