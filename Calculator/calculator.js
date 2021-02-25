82b21c11b7e0ab4b7e07a24ac32da1f6d55e3f84
Andy Dang <andy.dang.it@gmail.com>
2021-02-24 22:15:41 -0600

    Add app.get and app.listen

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("Hello World");
+});
+
+app.listen(3000, function(){
+    console.log("Server Started on port 3000");
+});
